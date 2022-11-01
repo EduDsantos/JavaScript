41137dbb30cc3135ca18d111306c8ea7c0a482a6
EduDsantos <eduardo.dev66@gmail.com>
2022-10-31 22:38:57 -0300

    Modelo

---
+++ Aula06/Ex14.js
@@ -0,0 +1,36 @@
+var agora = new Date()
+var DiaSem = agora.getDay()
+
+console.log(DiaSem)
+
+switch(DiaSem) {
+    case 0:
+        console.log('DOMINGO')
+        break
+    
+    case 1:
+        console.log('SEGUNDA')
+        break
+
+    case 2:
+        console.log('TERÇA')
+        break
+
+    case 3:
+        console.log('QUARTA')
+        break   
+
+    case 4:
+        console.log('QUINTA')
+        break
+    
+    case 5:
+        console.log('SEXTA')
+        break
+
+    case 6:
+        console.log('SABADO')
+        break
+        
+
+}
