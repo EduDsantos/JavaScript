9ae315d24d763932fb7daaae3a89da89bb69fe94
Luiz <96671069+lernenCode@users.noreply.github.com>
2023-01-11 20:15:56 -0300

    Update script.js

--- Aula13/script.js
+++ Aula13/script.js
@@ -1,16 +1,24 @@
-function clicar(){
-    var nuns = document.getElementById('txtn')
+function clicar() {
+    var nuns = document.getElementById('txtn');
+    var res = document.getElementById('res');
 
-    var res = document.getElementById('res')
+    if (nuns.value == 0 || nuns.value < 0 || nuns.value > 100) {
+        window.alert('ERRO! Insira um numero!');
+    } else {
+        let ar = [Number(nuns.value)];
 
-    if(nuns.value == 0 || nuns.value < 0 || nuns.value > 100 ){
-        window.alert('ERRO! Insira um numero!')
-    }else{
+        // Encontrar o valor máximo da array
+        let max = Math.max(...ar);
 
-        let ar = [Number(nuns.value)]
+        // Encontrar a soma de todos os valores na array
+        let sum = 0;
+        for (let i = 0; i < ar.length; i++) {
+            sum += ar[i];
+        }
 
-        var item = document.getElementById('option')
+        // Encontrar a média dos valores na array
+        let avg = sum / ar.length;
 
+        res.innerHTML = 'Valor máximo: ' + max + '<br>' + 'Soma: ' + sum + '<br>' + 'Média: ' + avg;
     }
-   
 }
