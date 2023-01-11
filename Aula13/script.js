acd3ccab8a71aeb6d1f5dba9ce21b5b57d8a30e6
EduDsantos <eduardo.dev66@gmail.com>
2023-01-11 20:07:22 -0300

    analisador de numeros

---
+++ Aula13/script.js
@@ -0,0 +1,16 @@
+function clicar(){
+    var nuns = document.getElementById('txtn')
+
+    var res = document.getElementById('res')
+
+    if(nuns.value == 0 || nuns.value < 0 || nuns.value > 100 ){
+        window.alert('ERRO! Insira um numero!')
+    }else{
+
+        let ar = [Number(nuns.value)]
+
+        var item = document.getElementById('option')
+
+    }
+   
+}
