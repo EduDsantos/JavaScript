d97df8b688e2c524af3f88c0429ff6d733592bde
EduDsantos <eduardo.dev66@gmail.com>
2023-01-25 19:12:06 -0300

    javaScript

--- Aula13/script.js
+++ Aula13/script.js
@@ -1,15 +1,54 @@
-function clicar(){
-    var nuns = document.getElementById('txtn')
+let num = document.getElementById('txtn')
+let lista = document.getElementById('listat')
+let resp = document.getElementById('res')
 
-    var res = document.getElementById('res')
+let valores = []
 
-    if(nuns.value == 0 || nuns.value < 0 || nuns.value > 100 ){
-        window.alert('ERRO! Insira um numero!')
+
+//Primeira parte: Adicionando numeros à lista.
+function clicar(){ 
+    if (num.value == 0 || num.value < 0 || num.value > 100){
+        window.alert('ERRO! Valores invalidos.')
     }else{
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        resp.innerHTML = ''
+    }
+    num.value = ''
+    num.focus()
+}
 
-        let ar = [Number(nuns.value)]
+//Segunda parte: Printando os resultados na tela.
 
-        var item = document.getElementById('option')
+function final(){
+    if(valores.length == 0){
+        window.alert('Adicione um valor')
+    }else{
+        let tot = valores.length
+
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma  = valores[0]
+        let media = valores[0]
+
+        for(let pos in valores){
+            soma += valores[pos]
+
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot        
+        resp.innerHTML = ''
+        resp.innerHTML +=`Ao todo temos ${tot} numeros adicionados!`
+
+        resp.innerHTML += `<p>o maior numero é o ${maior}</p>`
+        resp.innerHTML += `<p> o menor numero é o ${menor}</p>`
+        resp.innerHTML += `<p> A soma dos valores é de ${soma}</p>`
+        resp.innerHTML += `<p> A media dos valores é de ${media}</p>`
 
     }
    
