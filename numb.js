e7d80db3eced2c25b98c79b0e3988c2e7c4d2bb6
EduDsantos <eduardo.dev66@gmail.com>
2023-02-22 21:43:58 -0300

    numbers

---
+++ numb.js
@@ -0,0 +1,45 @@
+
+//CONVERTENDO A SAIDA DE UM NUMERO EM STRING
+
+/*let num1 = 10
+
+console.log(num1.toString(2))
+*/
+//----------------------------------------------------------------//
+
+//CONVERTENDO A VARIAVEL PARA UMA STRING PERMANENTEMENTE
+
+/*
+let num1 = 10
+
+num1 = num1.toString()
+
+console.log(typeof num1)
+*/
+//----------------------------------------------------------------//
+
+//ARREDONDANDO AS CASAS DECIMAIS DE ALGUM VALOR
+
+/*
+var num1 = 10.35848893
+
+console.log(num1.toFixed(2))
+*/
+
+//-----------------------------------------------------------------//
+
+//MOSTRANDO O VALOR BINARIO DE ALGUM NUMERO
+
+/*
+var num1 = 10
+
+console.log(num1.toString(2))
+*/
+
+//MOSTRANDO SE UM NUMERO É INTEIRO OU NAO
+
+/*
+var num1 = 10
+
+console.log(Number.isInteger(num1))
+*/
