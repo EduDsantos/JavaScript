8a57677b039c80eeb6caed473883152299021765
EduDsantos <eduardo.dev66@gmail.com>
2023-02-24 19:53:11 -0300

    MATH

---
+++ math.js
@@ -0,0 +1,53 @@
+//ARREDONDAMENTO//
+
+/*PARA BAIXO
+
+let num1 = 9.2324
+let num2 = Math.floor(num1)
+console.log(num2)
+
+*/
+
+/*PARA CIMA
+
+let num1 = 9.2324
+let num2 = Math.ceil(num1)
+console.log(num2)
+
+*/
+//-----------------------------------------------------------------------------//
+
+
+
+
+/* MENOR NUMERO DE UMA SEQUENCIA
+
+console.log(Math.max(1, 4,30,90,2))
+*/
+
+/* MENOR NUMERO DE UMA SEQUENCIA
+
+console.log(Math.min(1, 4,30,90,2))
+*/
+
+/* GERANDO NUMEROS ALEATORIOS DE UM VALOR A OUTRO
+
+console.log(Math.round(Math.random() * (0 + 100)))
+*/
+
+/* VALOR DE PI
+
+console.log(Math.PI)
+*/
+
+/* EXPONENCIAÇAO
+
+console.log(Math.pow(2, 10))
+*/
+
+/*RAIZ QUADRADA
+
+let num1 = 9
+console.log(num1 ** (1/2))  OU  console.log(num1 ** 0.5)
+
+*/
