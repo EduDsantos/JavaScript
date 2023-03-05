c7822097d0a331922025cff0eb6cf04d2cfd12c2
EduDsantos <eduardo.dev66@gmail.com>
2023-03-05 18:04:40 -0300

    function

---
+++ function/nome.js
@@ -0,0 +1,6 @@
+function saudacao(nome){
+    return `Bom dia ${nome}`
+}
+
+const variavel = saudacao('jorge')
+console.log(variavel)
---
+++ function/soma.js
@@ -0,0 +1,8 @@
+function soma(x,y){
+    const resultado = x + y
+    return resultado
+}
+
+console.log(soma(2,2))
+console.log(soma(2,7))
+console.log(soma(100,100))
