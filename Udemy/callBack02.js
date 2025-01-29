22c1fb72cd03a1c34ba4d96cf3e88655c9cf2925
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-01-28 21:13:36 -0300

    Studys

---
+++ Udemy/callBack02.js
@@ -0,0 +1,23 @@
+const notas = [4.1,5.1,7.1, 10]
+
+const notasBaixas =[]
+const notasAltas = []
+for(let i in notas){
+    if(notas[i] < 7){
+        notasBaixas.push(notas[i])
+    }else if(notas[i] > 7){
+        notasAltas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+console.log(notasAltas)
+
+
+//COM CALLBACK
+
+const notasBaixas2 = notas.filter(function (nota){
+    return nota < 7
+})
+
+console.log(notasBaixas2)
