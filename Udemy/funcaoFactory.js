76da06037834728c2a58ff77e0b697ca07e9d2f0
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-01-30 00:49:45 -0300

    FunctionFactory

---
+++ Udemy/funcaoFactory.js
@@ -0,0 +1,16 @@
+// function criarPessoa() {
+//     return {
+//         nome: 'eduardo',
+//         idade: 21
+//     }
+// }
+
+// console.log(criarPessoa())
+
+
+function criarPessoa(nome, idade, cidade){
+    return{nome,idade, cidade:'Sumaré-SP'} 
+}
+
+console.log(criarPessoa('Eduardo', 21))
+console.log(criarPessoa('Jorge', 22))
