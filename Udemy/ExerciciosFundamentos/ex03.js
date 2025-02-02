5e68bbb69447baca7b6fb2c564f8377886042792
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-02-02 20:40:31 -0300

    Exercicios

---
+++ Udemy/ExerciciosFundamentos/ex03.js
@@ -0,0 +1,15 @@
+function expoente(base,expoente){
+    
+    return  Math.pow(base,expoente)
+
+}
+
+console.log(expoente(2,2))
+console.log(expoente(5,5))
+
+
+function expoente2(base,expoente){
+    return base ** expoente
+}
+
+console.log(expoente2(5,5))
