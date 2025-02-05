a6f2fdd00dde2c75fa9bdb41cd0e4f0d07eae22f
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-02-05 00:29:57 -0300

    oo

---
+++ Udemy/ExerciciosFundamentos/funcoesImportantes.js
@@ -0,0 +1,12 @@
+const pessoa={
+    nome:'eduardo',
+    idade:2,
+    peso:10
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+//destruturing
+//DefineProperty
