035f02ea612ee67b8c6eba402b24fbc63366c769
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-02-07 23:47:41 -0300

    herança

--- Udemy/heranca.js
+++ Udemy/heranca.js
@@ -2,5 +2,16 @@ const ferrari ={
     modelo:'f40',
     vel:324
 }
+const volvo ={
+    modelo:'f40',
+    vel:324
+}
+
+
+
+
 
 console.log(ferrari.__proto__)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__)
