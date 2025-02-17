715ce23c2ae73203168ec6bfe0e6d76c026df98d
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-02-16 23:31:23 -0300

    node

---
+++ Udemy/ExerciciosFundamentos/ex07.js
@@ -0,0 +1,48 @@
+const mesNum = function(mes) {
+    switch (mes) {
+        case mes = 1:
+            console.log('janeiro')
+            break
+        case mes = 2:
+            console.log('fevereiro')
+            break
+        case mes = 3:
+            console.log('março')
+            break
+        case mes = 4:
+            console.log('abril')
+            break
+        case mes = 5:
+            console.log('maio')
+            break
+        case mes = 6:
+            console.log('junho')
+            break
+        case mes = 7:
+            console.log('julho')
+            break
+        case mes = 8:
+            console.log('agosto')
+            break
+        case mes = 9:
+            console.log('setembro')
+            break
+        case mes = 10:
+            console.log('outubro')
+            break
+        case mes = 11:
+            console.log('novembro')
+            break
+        case mes = 12:
+            console.log('dezembro')
+            break
+        default:
+            console.log("Mes invalido!")
+            break
+    }
+}
+
+console.log(mesNum(1))
+console.log(mesNum(2))
+console.log(mesNum(3))
+console.log(mesNum(12))
