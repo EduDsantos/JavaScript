549443423705624b4a05e34cf74e43c312707353
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2025-02-24 06:00:20 -0300

    forOf

--- Udemy/EsNext/forOf.js
+++ Udemy/EsNext/forOf.js
@@ -1,3 +1,24 @@
 for (let letra of 'Eduardo'){
     console.log(letra)
 }
+
+const assuntosMap = new Map([
+    ['Map', {abordado: true}],
+    ['Set', {abordado: true}],
+    ['Promise', {abordado:false}]
+])
+
+for (let assunto of assuntosMap){
+    console.log(assunto)
+}
+
+for(let chave of assuntosMap.keys()){
+    console.log(chave)
+}
+for(let value of assuntosMap.values()){
+    console.log(value)
+}
+
+for(let [ch,vl] of assuntosMap.entries()){
+    console.log(ch, vl)
+}
